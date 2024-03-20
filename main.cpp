e1b0ed45335b6dc475f112530087e04da9eef69f
Debanil Chowdhury <cdebanil@gmail.com>
2024-03-21 04:20:24 +0530

    remove unnecessary lines

--- main.cpp
+++ main.cpp
@@ -10,6 +10,8 @@
 #define RUNLENGTHENCODING 3
 #define DICTIONARYCODING 4
 
+using namespace std;
+
 struct rleCharNumPair{     
     unsigned char ch;        
     unsigned long long int count; 
@@ -17,13 +19,13 @@ struct rleCharNumPair{
 
 namespace rle
 {
-    std::string ex = "_rle"; 
+    string ex = "_rle"; 
 
-    void compress(std::vector<unsigned char> &input, std::vector<unsigned char> &output)
+    void compress(vector<unsigned char> &input, vector<unsigned char> &output)
     {
-        std::vector<rleCharNumPair> arr;
+        vector<rleCharNumPair> arr;
 
-        std::vector<unsigned char>::iterator it = input.begin();
+        vector<unsigned char>::iterator it = input.begin();
         unsigned char character = *it;
         it++;
         unsigned long long int counter = 1;
@@ -57,7 +59,7 @@ namespace rle
         }
         
         // Push all characters and their frequencies to output buffer
-        for (std::vector<rleCharNumPair>::iterator iter = arr.begin(); iter != arr.end(); iter++)
+        for (vector<rleCharNumPair>::iterator iter = arr.begin(); iter != arr.end(); iter++)
         {
             rleCharNumPair chN = *iter;
 
@@ -65,7 +67,7 @@ namespace rle
             output.push_back(chN.ch);
 
             // Convert number to null terminated string of unsigned char and push each to output buffer
-            char * num = std::to_string(chN.count).data();
+            char * num = to_string(chN.count).data();
             char * x = num;
             while (1)
             {
@@ -89,12 +91,12 @@ namespace rle
         
     }
 
-    void deCompress(std::vector<unsigned char> &input, std::vector<unsigned char> &output)
+    void deCompress(vector<unsigned char> &input, vector<unsigned char> &output)
     {
-        std::vector<unsigned char>::iterator it = input.begin();
+        vector<unsigned char>::iterator it = input.begin();
         while (it != input.end())
         {
-            std::vector<unsigned char> chNPairs;
+            vector<unsigned char> chNPairs;
             if (*it == '\0')
             {
                 chNPairs.push_back(*it);
@@ -107,7 +109,7 @@ namespace rle
             }
             unsigned char ch = chNPairs.at(0);
             chNPairs.erase(chNPairs.begin());
-            std::string str(chNPairs.begin(), chNPairs.end());
+            string str(chNPairs.begin(), chNPairs.end());
             unsigned long long int num = strtoull(str.c_str(), NULL, 10);
             for (unsigned long long int i = 0; i < num; i++)
             {
@@ -120,19 +122,19 @@ namespace rle
 
 namespace dc
 {
-    std::string ex = "_dc"; 
+    string ex = "_dc"; 
 
-    void compress(std::vector<unsigned char> &input, std::vector<unsigned char> &output)
+    void compress(vector<unsigned char> &input, vector<unsigned char> &output)
     {
-        for (std::vector<unsigned char>::iterator it = input.begin(); it != input.end(); it++)
+        for (vector<unsigned char>::iterator it = input.begin(); it != input.end(); it++)
         {
             output.push_back(*it);
         }
     }
 
-    void deCompress(std::vector<unsigned char> &input, std::vector<unsigned char> &output)
+    void deCompress(vector<unsigned char> &input, vector<unsigned char> &output)
     {
-        for (std::vector<unsigned char>::iterator it = input.begin(); it != input.end(); it++)
+        for (vector<unsigned char>::iterator it = input.begin(); it != input.end(); it++)
         {
             output.push_back(*it);
         }
@@ -147,9 +149,9 @@ int main()
     int argProcess = DECOMPRESS;
     // int argAlgorithm = DICTIONARYCODING;  
 
-    std::string extension = "_copy";
-    std::string inputFileName = "./testfiles/TEST";
-    std::string outputFileName = "./testfiles/TEST";
+    string extension = "_copy";
+    string inputFileName = "./testfiles/TEST";
+    string outputFileName = "./testfiles/TEST";
 
     if (argAlgorithm == RUNLENGTHENCODING)
     {
@@ -166,20 +168,20 @@ int main()
     {
         if (argProcess == COMPRESS)
         {
-            inputFileName = "./testfiles/TEST" + std::to_string(i);
+            inputFileName = "./testfiles/TEST" + to_string(i);
             outputFileName = inputFileName + extension;
         }
         if (argProcess == DECOMPRESS)
         {
-            inputFileName = "./testfiles/TEST" + std::to_string(i) + extension;
+            inputFileName = "./testfiles/TEST" + to_string(i) + extension;
             outputFileName = inputFileName + "_decompressed";
         }
 
-        std::ifstream input( inputFileName, std::ios::binary );
-        std::ofstream output( outputFileName, std::ios::binary ); 
+        ifstream input( inputFileName, ios::binary );
+        ofstream output( outputFileName, ios::binary ); 
 
-        std::vector<unsigned char> inputDataBuffer(std::istreambuf_iterator<char>(input), {});
-        std::vector<unsigned char> outputDataBuffer; 
+        vector<unsigned char> inputDataBuffer(istreambuf_iterator<char>(input), {});
+        vector<unsigned char> outputDataBuffer; 
 
 
         if (argProcess == COMPRESS)
@@ -205,7 +207,7 @@ int main()
             }
         }
         
-        std::copy(outputDataBuffer.begin(), outputDataBuffer.end(), std::ostreambuf_iterator<char>(output));
+        copy(outputDataBuffer.begin(), outputDataBuffer.end(), ostreambuf_iterator<char>(output));
   
     }
 }
